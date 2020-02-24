6b20a1f46f4c666fa1ba75afad628fa680d611bc
Bryan Valerio <bryanvale91@gmail.com>
2020-02-24 14:17:33 -0600

    Add strip-button component for payment processor

--- crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_HCt4VfbVeiDc3M5ozr7IJT0f00f5BV8g7z';
 
   const onToken = token => {
     console.log(token);
